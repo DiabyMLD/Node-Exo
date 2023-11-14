e082ae4a00a8752039d85f57dfed5df963fd276c
DIABY Hassane <mlamine.diaby01@gmail.com>
2023-11-14 11:02:30 +0100

    newTP2

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 var express = require('express');
 const { bizareDB } = require('./config/database');
-
+const twig = require('twig');
 
 var app = express();
 
 // Connexion à la base de données
 bizareDB();
 
+app.set('view engine', 'twig');
+app.set('views', './views');
+
 // Route de base pour tester
 app.get('/', (req, res) => {
   res.send('Yeah ! Ça marche ! Je suis dans le navigateur ! 🎉🚀');
+});
+
+app.get('/home', (req, res) => {
+  res.render('home');
 });
 
 app.listen(3333, () => {
