b701283f1d2e37d23a2b5db68b6a3eb32e9716d6
DIABY Hassane <mlamine.diaby01@gmail.com>
2023-11-15 10:45:16 +0100

    TP7 corrected

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,37 @@
     // Importation du modèle User
     const User = require('../models/User');
+      // Importation du module multer
+       // Importation du module multer
+    const multer = require('multer');
+
+    // Configuration de multer
+    const storage = multer.memoryStorage();
+    const upload = multer({ storage: storage });
 
     // Affichage de la page d'inscription
     exports.showRegisterPage = (req, res) => {
         res.render('pages/register');
     };
 
-    // Enregistrement d'un nouvel utilisateur
+       // Enregistrement d'un nouvel utilisateur
     exports.registerUser = async (req, res) => {
-        const { username, email, password } = req.body;
-        const newUser = new User({ username, email, password });
-        await newUser.save();
-        res.redirect('/users');
+        try {
+            const { username, email, password } = req.body;
+            const newUser = new User({
+                username,
+                email,
+                password,
+                avatar: {
+                    data: req.file.buffer,
+                    contentType: req.file.mimetype
+                }
+            });
+            await newUser.save();
+            res.redirect('/users');
+        } catch (error) {
+            console.error("Erreur lors de l'enregistrement de l'utilisateur :", error);
+            res.status(500).send("Erreur lors de l'enregistrement de l'utilisateur.");
+        }
     };
 
       // Affichage de la page de modification d'un utilisateur
@@ -21,11 +41,36 @@
     };
     // Modification d'un utilisateur
     exports.editUser = async (req, res) => {
-        const { username, email, password } = req.body;
-        await User.findByIdAndUpdate(req.params.id, { username, email, password });
-        res.redirect('/users');
-    };
+        try {
+            // Log pour vérifier les données reçues
+            console.log("Données du formulaire reçues:", req.body);
+            console.log("Fichier reçu:", req.file);
+            const { username, email, password } = req.body;
+            const updateData = { username, email, password };
 
+            // Vérifie si un fichier a été envoyé
+            if (req.file) {
+                updateData.avatar = {
+                    data: req.file.buffer,
+                    contentType: req.file.mimetype
+                };
+            } else {
+                console.log("Aucun fichier envoyé");
+            }
+
+            // Met à jour l'utilisateur
+            await User.findByIdAndUpdate(req.params.id, updateData);
+
+            // Redirige vers la liste des utilisateurs
+            res.redirect('/users');
+        } catch (error) {
+            // Log d'erreur
+            console.error("Erreur lors de la modification de l'utilisateur :", error);
+
+            // Envoie une réponse d'erreur
+            res.status(500).send("Erreur lors de la modification de l'utilisateur.");
+        }
+    };
     // Suppression d'un utilisateur
     exports.deleteUser = async (req, res) => {
         await User.findByIdAndDelete(req.params.id);
@@ -37,3 +82,5 @@
         const users = await User.find();
         res.render('pages/users', { users });
     };
+
+    
--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,45 @@
 const express = require('express');
 const router = express.Router();
-// Importation du contrôleur utilisateur
 const userController = require('../controllers/userController');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+const User = require('../models/User');
+router.post('/register', upload.single('avatar'), userController.registerUser);
+// Routes utilisateur
 
-// Définition des routes utilisateur
+
+// Afficher la page d'inscription
 router.get('/register', userController.showRegisterPage);
-router.post('/register', userController.registerUser);
+
+// Lister tous les utilisateurs
 router.get('/users', userController.listUsers);
- // Route pour afficher la page de modification
- router.get('/edit/:id', userController.showEditPage);
 
- // Route pour modifier un utilisateur
- router.post('/edit/:id', userController.editUser);
+// Afficher la page de modification
+router.get('/edit/:id', userController.showEditPage);
 
- // Route pour supprimer un utilisateur
- router.get('/delete/:id', userController.deleteUser);
+// Supprimer un utilisateur
+router.get('/delete/:id', userController.deleteUser);
+
+// Enregistrer un nouvel utilisateur
+router.post('/register', upload.single('avatar'), userController.registerUser);
+
+// Modifier un utilisateur
+router.post('/edit/:id', upload.single('avatar'), userController.editUser);
+
+// Servir les images (avatars)
+router.get('/avatar/:id', async (req, res) => {
+    try {
+        console.log("Requête pour l'avatar reçue"); // Log
+        const user = await User.findById(req.params.id);
+        console.log("Utilisateur trouvé :", user); // Log
+        res.set('Content-Type', user.avatar.contentType);
+        res.send(user.avatar.data);
+    } catch (error) {
+        console.error("Erreur :", error); // Log d'erreur
+        res.status(400).send("Erreur lors de la récupération de l'avatar");
+    }
+});
 
 // Exportation du routeur
 module.exports = router;
